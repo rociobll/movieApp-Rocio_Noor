b80523cee1fd0c32cbdfd0292fe8f58704f286b5
Noor A. S. <176476609+noor290602@users.noreply.github.com>
2025-06-02 16:14:00 +0200

    UPDATE(mockup página series): html con Boostrap 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MoviesComponent } from "./pages/movies/movies.component";
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MoviesComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
